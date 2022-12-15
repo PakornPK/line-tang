475a60300ee78ed2c9dff96e20ee1b8905f03736
pakornpk <sweetie_jimmie@gmail.com>
2022-12-15 16:07:41 +0700

    add: viper for manage config

---
+++ src/configs/config.go
@@ -0,0 +1,29 @@
+package configs
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+var port string
+
+func LoadConfig() error {
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+	viper.AddConfigPath("src")
+	err := viper.ReadInConfig()
+	if err != nil {
+		return err
+	}
+	setPort(viper.Get("PORT"))
+	return err
+}
+
+func setPort(p interface{}) {
+	port = fmt.Sprintf(":%s", p)
+}
+
+func GetPort() string {
+	return port
+}
--- src/main.go
+++ src/main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"log"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/pakornpk/line-tang/src/configs"
 	"github.com/pakornpk/line-tang/src/routers"
 )
 
 func main() {
+	err := configs.LoadConfig()
+	if err != nil {
+		log.Fatalf("error: %s", err.Error())
+	}
+
 	e := echo.New()
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-	
+
 	routers.Router(e)
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(configs.GetPort()))
 }
